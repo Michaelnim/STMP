d6ab670caffe19fdc6d924a138a8da330abc53c1
Michael Vo <vo.mikey@gmail.com>
2023-04-10 11:30:14 -0700

    Added a with open to read the png file

--- STMP.py
+++ STMP.py
@@ -15,10 +15,16 @@ body = "Hey there! I'm learning to send emails using Python!"
 message.set_content(body)
 
 
-attachment_path = (r"C:\Users\Bmo\Pictures\Saved Pictures\test.png")
+attachment_path = (r"C:\Users\Bmo\Pictures\Saved Pictures\test2.png")
 attachment_filename = os.path.basename(attachment_path)
 mime_type, _ = mimetypes.guess_type(attachment_path)
 mime_type, mime_subtype = mime_type.split('/', 1)
 print(mime_type)
 print(mime_subtype)
 
+with open(attachment_path, 'rb') as ap:
+     message.add_attachment(ap.read(),
+                            maintype=mime_type,
+                            subtype=mime_subtype,
+                            filename=os.path.basename(attachment_path))
+print(message)
